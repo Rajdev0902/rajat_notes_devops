531baa0389927b71a11ccede172e648cb0658c0f
@rajatgithub <rajatchaturvedi59@gmail.com>
2025-08-24 21:18:58 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,14 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "4.34.0"
+      source = "hashicorp/azurerm"
+      version = "4.40.0"
     }
   }
 }
-
 provider "azurerm" {
   features {}
-  subscription_id = "8f765261-0137-4fd7-b8de-53247b5236d0"
+  subscription_id = "18f8e7c5-a3db-4324-b49d-7ef07eace03f"
 }
 
 resource "azurerm_resource_group" "Rajatrg" {
@@ -63,28 +62,5 @@ resource "azurerm_network_interface" "nic" {
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.public_ip.id
-  }
-}
-
-resource "azurerm_key_vault" "kv" {
-  name                        = "rajat-keyvault01"
-  location                    = azurerm_resource_group.Rajatrg.location
-  resource_group_name         = azurerm_resource_group.Rajatrg.name
-  tenant_id                   = "YOUR_TENANT_ID" // Replace with your Azure AD tenant ID
-  sku_name                    = "standard"
-  soft_delete_retention_days  = 7
-  enabled_for_disk_encryption = true    
-  purge_protection_enabled    = false 
-
-  access_policy {
-    tenant_id = "3c8faa8b-263e-416d-bbb8-6eea957127df" 
-    object_id = "YOUR_OBJECT_ID" 
-    secret_permissions = [
-      "get"
-    ]
-    key_permissions = [
-      "get"
-      
-    ]
   }
 }
